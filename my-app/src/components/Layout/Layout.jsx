20a84ca83696b45a261a5085d014eeadd3f734ad
SangatiThanmayee <reddysangatithanmayee@gmail.com>
2025-12-25 14:26:41 +0530

    header component with navigation styling

---
+++ my-app/src/components/Layout/Layout.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "../header/Header";
+
+
+
+
+
+const Layout = () => {
+  return (
+    <>
+<Header/>
+      
+    </>
+  );
+};
+
+export default Layout;
---
+++ my-app/src/context/AuthContext.jsx
@@ -0,0 +1,79 @@
+import { createContext, useEffect, useReducer } from "react";
+
+const initial_state = {
+  user:
+    localStorage.getItem("user") !== undefined
+      ? JSON.parse(localStorage.getItem("user"))
+      : null,
+  loading: false,
+  error: null,
+  role: localStorage.getItem('role') || null,
+  token: localStorage.getItem('token') || null,
+};
+
+export const AuthContext = createContext(initial_state);
+const AuthReducer = (state, action) => {
+  console.log(action.token)
+  console.log(action.payload)
+  switch (action.type) {
+    case "LOGIN_START":
+      return {
+        user: null,
+        token: null,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_SUCCESS":
+      return {
+        user: action.payload,
+        token: action.token,
+        role: action.role,
+        loading: false,
+        error: null,
+      };
+    case "LOGIN_FAILURE":
+      return {
+        user: null,
+        loading: false,
+        error: action.payload,
+      };
+    case "REGISTER_SUCCESS":
+      return {
+        user: null,
+        loading: false,
+        error: null,
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+        role: null,
+        token: null,
+        loading: false,
+        error: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(AuthReducer, initial_state);
+
+  useEffect(() => {
+    localStorage.setItem("user", JSON.stringify(state.user));
+    localStorage.setItem('token', state.token);
+    localStorage.setItem('role', state.role);
+  }, [state]);
+  return (
+    <AuthContext.Provider
+      value={{
+        user: state.user,
+        loading: state.loading,
+        error: state.error,
+        dispatch,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
